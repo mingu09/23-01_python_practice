931fc589f485bdcb048746927ccf18008cc6d12f
dlaanfdl <sh5183629@gmail.com>
2023-03-19 12:28:04 +0900

    :memo:update

---
+++ algo/if/14681.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+if x > 0 and y > 0:
+    print("Quadrant 1")
+elif x < 0 and y > 0:
+    print("Quadrant 2")
+elif x < 0 and y < 0:
+    print("Quadrant 3")
+else:
+    print("Quadrant 4")
