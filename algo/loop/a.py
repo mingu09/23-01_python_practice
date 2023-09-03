76080b9ba3a2d041b3671432a9e9f17f8322f26c
mingu09 <mingustar09@gmail.com>
2023-09-03 11:51:42 +0900

    :memo:update

---
+++ algo/loop/a.py
@@ -0,0 +1,22 @@
+# for
+# while
+
+# for i in range(10):
+#     print(i+1)
+
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1 
+
+# for i in range(100):
+#     if i == 10:
+#         break
+#     print(i)
+
+
+while True:
+    a, b = map(int,input().split())
+    if a == 0:
+        break
+    print(a, b)
